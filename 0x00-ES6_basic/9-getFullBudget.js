6b5b334d547b7560328df5bdd7f278364919d7ed
austin <trigger_austin@proton.me>
2024-12-24 14:44:34 +0300

    late submission

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,20 @@
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
